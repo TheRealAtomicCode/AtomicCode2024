f86feca9eaef3ea1d586adbbe749f9d29264d63c
Abd al Qader Baghi <aqbaghi@atomiccode.uk>
2024-04-22 22:09:43 +0200

    added extra text and added rtl on the arabic

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,66 +1,101 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+	import { ref } from 'vue';
 
-const textIndex = ref<0 | 1 | 2>(0);
+	const textIndex = ref<0 | 1 | 2 | 3 | 4>(0);
 
-const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890!@#$%^&*()_-';
+	const letters =
+		'ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890!@#$%^&*()_-卡德尔卡迈勒';
 
-const listOfTitles: string[] = ['ATOMIC CODE', 'QADER KAMAL'];
+	const listOfTitles: string[] = [
+		'ATOMIC CODE',
+		'QADER KAMAL',
+		'ATOMIC CODE',
+		'  عبدالقادر',
+		'原子密码       ',
+	];
 
-const name = ref<string>('ATOMIC CODE');
+	const name = ref<string>('ATOMIC CODE');
 
-const changeText = () => {
-  let iterations = 1 / 3;
+	const changeText = () => {
+		let iterations = 1 / 4;
 
-  textIndex.value += 1;
-  if (textIndex.value === 2) {
-    textIndex.value = 0;
-  }
+		textIndex.value += 1;
+		if (textIndex.value === 5) {
+			textIndex.value = 0;
+		}
 
-  const interval = setInterval(() => {
-    const word = name.value
-      .split('')
-      .map((letter, index) => {
-        if (index < iterations) {
-          return listOfTitles[textIndex.value][index];
-        }
+		const interval = setInterval(() => {
+			const word = name.value
+				.split('')
+				.map((letter, index) => {
+					if (index < iterations) {
+						return listOfTitles[textIndex.value][index];
+					}
 
-        return letters[Math.floor(Math.random() * 48)];
-      })
-      .join('');
-    name.value = word;
+					return letters[Math.floor(Math.random() * 48)];
+				})
+				.join('');
+			name.value = word;
 
-    if (iterations >= listOfTitles[textIndex.value].length)
-      clearInterval(interval);
+			if (iterations >= listOfTitles[textIndex.value].length)
+				clearInterval(interval);
 
-    iterations += 1;
-  }, 40);
-};
+			iterations += 1;
+		}, 40);
+	};
 </script>
 
 <template>
-  <div class="bg-gray-900">
-    <h1
-      class="h-[100vh] text-center flex items-center justify-center text-6xl bg-gradient-to-tr from-white to-teal-200 bg-clip-text text-transparent relative font-mono font-extrabold z-20"
-    >
-      <span @mouseover="changeText">{{ name }}</span
-      ><span class="cursor-block"></span>
-    </h1>
-  </div>
+	<div class="bg-gray-900">
+		<h1
+			:dir="textIndex !== 3 ? 'ltr' : 'rtl'"
+			:class="
+				textIndex === 0 || textIndex === 1 || textIndex === 4
+					? 'eng'
+					: textIndex === 3
+					? 'ar'
+					: 'bar'
+			"
+			class="h-[100vh] text-center flex items-center justify-center text-7xl bg-gradient-to-tr from-white to-white bg-clip-text text-transparent relative z-20"
+		>
+			<span @mouseover="changeText">{{ name }}</span
+			><span v-if="textIndex !== 2" class="cursor-block"></span>
+		</h1>
+	</div>
 </template>
 
 <style>
-.cursor-block {
-  display: inline-block;
-  width: 0.55em;
-  height: 1em;
-  background-color: rgb(255, 255, 255);
-  animation: blink 0.7s steps(1, start) infinite;
-}
+	@import url('https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&family=Jersey+10&family=Jersey+25&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&family=Jersey+10&family=Jersey+25&family=Libre+Barcode+39+Extended+Text&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&family=Jersey+10&family=Jersey+25&family=Libre+Barcode+39+Extended+Text&family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap');
+	.cursor-block {
+		display: inline-block;
+		width: 0.55em;
+		height: 1em;
+		background-color: rgb(255, 255, 255);
+		animation: blink 0.7s steps(1, start) infinite;
+	}
 
-@keyframes blink {
-  50% {
-    opacity: 0;
-  }
-}
+	@keyframes blink {
+		50% {
+			opacity: 0;
+		}
+	}
+
+	.ar {
+		font-family: 'Cairo', sans-serif;
+		font-optical-sizing: auto;
+		font-weight: 700;
+	}
+
+	.bar {
+		font-family: 'Libre Barcode 39 Extended Text', system-ui;
+		font-weight: 400;
+		font-style: normal;
+	}
+
+	.eng {
+		font-family: 'Roboto Flex', sans-serif;
+		font-optical-sizing: auto;
+	}
 </style>
