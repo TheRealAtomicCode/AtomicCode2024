aba9262e6a4053017a9f6b91f64e2f7af2ef6ae5
Abd Al Qader Baghi <a.baghi@MITT.LY>
2024-04-22 12:16:37 +0200

    added the change effect when hovering over the title text

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,47 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+
+const textIndex = ref<0 | 1 | 2>(0);
+
+const listOfTitles: string[] = [
+  'Atomic Code',
+  'Qader Kamal',
+  'Timmy terminal',
+];
+
+const changeText = function () {
+  textIndex.value++;
+  if (textIndex.value == 3) {
+    textIndex.value = 0;
+  }
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="bg-black">
+    <h1
+      class="h-[100vh] text-center flex items-center justify-center text-6xl bg-gradient-to-tr from-green-500 to-teal-700 bg-clip-text text-transparent relative font-mono font-extrabold"
+    >
+      <span @mouseover="changeText">{{
+        listOfTitles[textIndex]
+      }}</span
+      ><span class="cursor-block"></span>
+    </h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+.cursor-block {
+  display: inline-block;
+  width: 0.55em;
+  height: 1em;
+  background-color: rgb(10, 127, 63);
+  animation: blink 0.7s steps(1, start) infinite;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+@keyframes blink {
+  50% {
+    opacity: 0;
+  }
 }
 </style>
